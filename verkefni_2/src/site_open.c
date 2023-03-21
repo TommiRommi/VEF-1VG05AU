38ea6837a7a08f548732f17e942f64e0adfac3dc
TommiRommi <tomasgardar2010@gmail.com>
2023-03-21 14:47:00 +0000

    added comments

--- verkefni_2/src/site_open.c
+++ verkefni_2/src/site_open.c
@@ -10,10 +10,12 @@
 #define Break	"/"
 #endif
 
+// path to html file
 #define Html_File_Path 	"html" Break "website.html"
 #define Html_Open		Comm " " Html_File_Path
 #define Path(x, y)	x Break y
 
+// path to all project files
 const char *project_files[] = {
 	Html_File_Path, Path("css", "stylesheet.css")
 }; 
@@ -24,6 +26,7 @@ int main(void)
 	errno = 0;
 	FILE *file_ptr;
 
+	// checks if every file in the project exist, else throw a error
 	for(int x = 0; x < (int)sizeof(project_files) >> 3; x++)
 	{
 		if((file_ptr = fopen(project_files[x], "r")) == NULL)
@@ -37,6 +40,7 @@ int main(void)
 		}
 	}
 
+	// opens the html file as a new window in the default web browser
 	if(system(Html_Open) == -1)
 	{
 		fprintf(stderr, "something went wrong while executing shell command");	
